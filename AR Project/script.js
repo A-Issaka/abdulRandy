3ece4b024060d1fca4591f7c4e8aca2bb290a767
RbHuerta09 <randyh36@nycstudents.net>
2024-12-18 14:39:56 -0500

    Add files via upload

--- AR Project/script.js
+++ AR Project/script.js
@@ -7,14 +7,3 @@ ctx.lineTo(300,100);
 ctx.lineWidth = 5;
 ctx.stroke();
 
-
-window.addEventListener('scroll', function() {
-    var titleContainer = document.getElementById('title-container');
-    var scrollPosition = window.scrollY;
-    
-    if (scrollPosition > 0) {
-      titleContainer.style.height = '10px'; // Adjust this value to your preferred shrunk height
-    } else {
-      titleContainer.style.height = '100vh';
-    }
-  });
